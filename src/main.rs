04e6e765ee06fd65ec85f07d46929c0a50f338d5
kylekibet <kyleomondi@gmail.com>
2025-11-17 11:28:29 +0300

    add conversion functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let degree_sign = '\u{00B0}';
+    let f = f_to_c(80.0);
+    let c = c_to_f(f);
+    println!("{f:.2}{}C", degree_sign);
+    println!("{c:.2}{degree_sign}F");
+}
+
+
+fn f_to_c (num :f64) -> f64 {
+    (num - 32.0) / 1.8
+}
+
+fn c_to_f(num :f64) -> f64 {
+    (num * 1.8) + 32.0
 }
